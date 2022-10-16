aa8a256575c7b5bf1812efef6fdc1eb8bbc18a07
RuthKago <113931463+RuthKago@users.noreply.github.com>
2022-10-16 23:09:07 +0300

    Add files via upload

---
+++ ex5.py
@@ -0,0 +1,19 @@
+my_name = 'Zed A.Shaw'
+my_age = 35 #It's a lie
+my_height = 74 # inches
+my_weight = 180 # lbs # pounds
+my_eyes = 'Blue'
+my_teeth = 'White'
+my_hair = 'Brown'
+
+
+print(f"Let's talk about {my_name},")
+print(f"He's {my_height} inches tall.")
+print(f"He's {my_weight} pounds heavy.")
+print("Actually that's not too heavy")
+print(f"He's not{my_eyes} eyes and {my_hair} hair.")
+print(f"His teeth are usually {my_teeth} depending on the coffee.")
+
+#this line is tricky, try to get the exactly right
+total = my_age + my_height + my_weight
+print(f"If I add {my_age}, {my_height}, and {my_weight} I get {total}.") #Differrent units
---
+++ ex7.py
@@ -0,0 +1,22 @@
+#Exercise 7
+
+print("Mary had a little lamb.")
+print("Its fleece was white as {}.".format('snow')) # Snow is not a variable
+print("." * 10)
+
+end1 = "c"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+
+print (end1 + end2 + end3 + end4 + end5 + end6, end= ' ') # Adding the end=' ' combines the two lines with a plus sign
+print (end7 + end8 + end9 + end10 + end11 + end12)
